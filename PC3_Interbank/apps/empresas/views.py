2012e290d99945387d1db0d710416ceb4b488a26
Kjfer <Gfernandez_06@outlook.com>
2025-06-05 23:18:44 -0500

    que mrda es un token

--- PC3_Interbank/apps/empresas/views.py
+++ PC3_Interbank/apps/empresas/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class PanelEmpresaView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if request.user.rol != 'empresa':
+            return Response({'error': 'Acceso no autorizado'}, status=403)
+
+        return Response({
+            "mensaje": f"Hola {request.user.nombre}, bienvenido al panel de empresa.",
+            "rol": request.user.rol
+        })
--- PC3_Interbank/apps/users/urls.py
+++ PC3_Interbank/apps/users/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import PanelEmpresaView
 
-urlpatterns = []
+urlpatterns = [
+    path('panel/', PanelEmpresaView.as_view(), name='panel_empresa'),
+]
